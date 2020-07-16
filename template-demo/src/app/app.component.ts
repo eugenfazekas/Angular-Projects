1b9a1e03a29436dee9d9ddb773285fc37a624847
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-07-16 17:32:25 +0300

    Template-Demo Practice: bindings, getters-setters

--- template-demo/src/app/app.component.ts
+++ template-demo/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'template-demo';
+export class AppComponent implements AfterViewInit {
+  pageTitle = 'Angular Component Interaction';
+  imgUrl = 'https://picsum.photos/200';
+  count = 0;
+  name: string;
+  userName: string;
+  private _customerName: string;
+  @ViewChild('nameRef') nameElementRef: ElementRef;
+
+  ngAfterViewInit() {
+    this.nameElementRef.nativeElement.focus();
+  }
+
+  get customerName(): string {
+    return this._customerName;
+  }
+
+  set customerName(value: string) {
+    this._customerName = value;
+    if(value === 'Vishwas'){
+      alert('Hello Vishwas');
+    }
+  }
+
+  incrementCount(){
+    this.count += 1;
+  }
+  greetVishwas(updatedValue)   {
+    this.userName = updatedValue;
+    if(updatedValue === 'Vishwas'){
+      alert('Welcome back Vishwas');
+    }
+  }
+
 }
