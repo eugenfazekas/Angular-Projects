fd4081b6eead871fcc3191dd245131e953271313
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-07-18 18:41:16 +0300

    Added Material examples: typograhy,button,togglebutton,icons,badges,progress spinner,navbar,sidenav

--- material-demo/src/app/app.component.ts
+++ material-demo/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+  notifications = 0;
+  showSpinner = false;
+  opened = false;
+  loadData() {
+    this.showSpinner = true;
+    setTimeout(() => {this.showSpinner = false ;},5000);
+  }
+  log(state) {
+    console.log(state)
+  }
 }
--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
-  ],
+    ,FormsModule,
+    MaterialModule
+     ],
   providers: [],
   bootstrap: [AppComponent]
 })
