316232d44193b2b9da569171a454e449e05cb96c
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-07-21 22:56:08 +0300

    added template-driven-form practice example

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'template-driven-forms';
+  topics = ['Angular','React','Vue'] ;
+  topicHasError = true;
+  submitted = false;
+  userModel = new User('','rob@text.com',555666554,'default','morning',true);
+  errorMsg = '';
+
+  constructor (private _enrollmentService :EnrollmentService) {
+
+  }
+ 
+  validateTopic(value){
+    if(value == 'default') {
+      this.topicHasError = true;
+    }else {
+      this.topicHasError = false;
+    }
+  }
+  onSubmit(){
+    this.submitted = true;
+    this._enrollmentService.enroll(this.userModel)
+    .subscribe(
+      data => console.log('Succes!',data),
+      error => this.errorMsg = error.statusText
+      
+    )
+  }
 }
