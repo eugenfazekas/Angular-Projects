a5b30bb33e8ba75c84d9efc399cf091f5eeffe38
Eugen Fazekas <eugen.fazekas@gmail.com>
2020-07-31 21:19:00 +0300

    added authentication practice example with mongodb

--- angularauth/server/server.js
+++ angularauth/server/server.js
@@ -1,1 +1,21 @@
-const express = require('express')const bodyParser = require('body-parser')const PORT = 3000const app = express()app.use(bodyParser.json())app.get('/',function(req,res) {	res.send('Hello from server')})app.listen(PORT,function() {	console.log('Server running on localhost:' + PORT)}) 
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const PORT = 3000
+const api = require('./routes/api')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/api',api)
+app.get('/',function(req,res) {
+	res.send('Hello from server')
+})
+
+app.listen(PORT,function() {
+	console.log('Server running on localhost:' + PORT)
+})
+
+ // mongodb+srv://mlabuserjohn:<password>@eventsdb.v3lxj.mongodb.net/<dbname>?retryWrites=true&w=majority
